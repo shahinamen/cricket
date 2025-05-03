55437d4076e33489c802bb7db1e36e00397a5f19
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-05-03 18:13:22 +0600

    menu adding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,14 @@ export default function Header() {
     return (
         <header className="bg-sky-500 text-white p-2 text-center">            
             <ul className="flex flex-col sm:flex-row sm:space-x-4 text-xs items-center">
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">Matches (16)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">BBL (4)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">PSL (3)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">SL-U19 v BDESH-U19 (1)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">County DIV1 (3)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">County DIV2 (4)</li>
-                <li className="bg-black py-1 px-2 rounded-sm mt-1">Womens Tri-Series (SL) (1)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">Matches (16)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">BBL (4)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">PSL (3)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">SL-U19 v BDESH-U19 (1)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">County DIV1 (3)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">County DIV2 (4)</li>
+                <li className="bg-black py-1 px-2 rounded-sm mt-1 cursor-pointer">Womens Tri-Series (SL) (1)</li>
             </ul>
-            <div>
-                <h1 className="text-4xl font-bold py-2">Cricket Scoreboard</h1>
-                <p className="text-sm">Live Scores, News, Stats, Videos, Schedule, Fixtures</p>
-            </div> 
         </header>
     )
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,15 @@
+export default function Menu() {  
+    return (
+        <div className="bg-sky-600 text-white p-2 text-center">            
+            <ul className="flex flex-col sm:flex-row sm:space-x-4 text-sm items-center">
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Live Scores</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Series</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Team</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">News</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Features</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Videos</li>
+                <li className="hover:bg-black py-1 px-2 cursor-pointer rounded-sm mt-1">Statistics</li>
+            </ul>
+        </div>
+    )
+}
