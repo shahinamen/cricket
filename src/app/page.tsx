4dbbb734a54d75a2246c8b967c4290c64ab39992
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-05-03 17:14:06 +0600

    header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
+import Header from "@/components/Header";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-center py-2">This is React Js Project</h1>
+      <Header />
     </div>
   );
 }
